e678d2b509a20dfe877b1d57bb8151cfb8b77e51
Phil Olson <olsonpm@users.noreply.github.com>
2022-09-13 20:13:16 -0500

    fix reset default values bug
    also handle bad setting input

--- src/options/index.mjs
+++ src/options/index.mjs
@@ -1,18 +1,31 @@
 import settingDefaults from '../setting-defaults.mjs'
 
+const playlistLimitEl = document.getElementById('playlist-limit')
+
 document.getElementById('settings-form').addEventListener('submit', e => {
   e.preventDefault()
 })
 
 document.getElementById('restore-defaults').addEventListener('click', () => {
   browser.storage.sync.set(settingDefaults)
+  setPlaylistLimitElValue()
 })
 
-browser.storage.sync.get('playlistLimit').then(({ playlistLimit }) => {
-  playlistLimitEl.value = playlistLimit
-})
-
-const playlistLimitEl = document.getElementById('playlist-limit')
 playlistLimitEl.addEventListener('blur', () => {
-  browser.storage.sync.set({ playlistLimit: playlistLimitEl.value })
+  const playlistLimitIsValid = playlistLimitEl.value?.match(/^\d+$/)
+  const playlistLimit = playlistLimitIsValid
+    ? playlistLimitEl.value
+    : settingDefaults.playlistLimit
+
+  browser.storage.sync.set({ playlistLimit })
+
+  if (!playlistLimitIsValid) playlistLimitEl.value = playlistLimit
 })
+
+setPlaylistLimitElValue()
+
+function setPlaylistLimitElValue() {
+  browser.storage.sync.get('playlistLimit').then(({ playlistLimit }) => {
+    playlistLimitEl.value = playlistLimit
+  })
+}
